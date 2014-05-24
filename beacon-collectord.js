9177a52bdc4016465f19155e6bb7eb943c8c1ef4
Alexander Meesters <a@m-id.me>
2014-05-24 02:07:26 +0200

    modified the db, added some fields and changed to a more consistend naming scheme.

--- beacon-collectord.js
+++ beacon-collectord.js
@@ -1,6 +1,8 @@
+#!/usr/bin/env node
+
 /**
  * Created by Alexander Meesters <a@m-id.me>
- *     iteration 9
+ *     iteration 11
  */
 
 var pcap = require("pcap"),
@@ -8,13 +10,22 @@ var pcap = require("pcap"),
     pcap_session = pcap.createSession("", "tcp");
 
 var ip = require('ip');
-
 var models = require('./models');
 
 console.log("Listening on: " + pcap_session.device_name);
 
 // what goes up:
 tcp_tracker.on('http request complete', function(session, http){
+
+    var logThis = new models.realtimeLog({
+        type: 'request',
+        srcIpAddress: ip.toLong(session.src),
+        destIpAddress: ip.toLong(session.dst),
+        requestMethod: http.request.method,
+        requestUrl: http.request.url,
+        contentType: 'N/A'
+    });
+
     console.log(session.src + " > " + session.dst +": " + http.request.url);
 });
 
@@ -22,10 +33,12 @@ tcp_tracker.on('http request complete', function(session, http){
 tcp_tracker.on('http response complete', function(session, http){
 
     var logThis = new models.realtimeLog({
-        ipAddress: ip.toLong(session.dst),
-        request_method: http.request.method,
-        request_url: http.request.url,
-        content_type: http.response.headers['Content-Type']
+        type: 'response',
+        srcIpAddress: ip.toLong(session.src),
+        destIpAddress: ip.toLong(session.dst),
+        requestMethod: http.request.method,
+        requestUrl: http.request.url,
+        contentType: http.response.headers['Content-Type']
     });
 
     logThis.save(function(err) {
@@ -41,10 +54,10 @@ pcap_session.on('packet', function(raw_packet){
     tcp_tracker.track_packet(packet);
 });
 
-    // Check for pcap dropped packets on an interval
-    setInterval(function () {
-        var stats = pcap_session.stats();
-        if (stats.ps_drop > 0) {
-            console.log(ANSI("pcap dropped packets, need larger buffer or less work to do: " + util.inspect(stats), "bold"));
-        }
-    }, 2000);
+// Check for pcap dropped packets on an interval
+setInterval(function () {
+    var stats = pcap_session.stats();
+    if (stats.ps_drop > 0) {
+        console.log(ANSI("pcap dropped packets, need larger buffer or less work to do: " + util.inspect(stats), "bold"));
+    }
+}, 2000);
--- models.js
+++ models.js
@@ -5,17 +5,14 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 var realtimeLogSchema = mongoose.Schema({
-    ipAddress: Number,
-    request_method: String,
-    request_url: String,
-    content_type: String
+	type: String,
+	srcIpAddress: Number,
+    destIpAddress: Number,
+    requestMethod: String,
+    requestUrl: String,
+    contentType: String
 });
-
 
 var realtimeLog = mongoose.model('realtimeLog', realtimeLogSchema);
 
-/*
-* As a guideline, if the relationship between exports and module.exports seems like magic to you,
-* ignore exports and only use module.exports.
-*/
 exports.realtimeLog = realtimeLog;
